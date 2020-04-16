b53c278e6c8ec424ac0c7f9a426dc06e63be2b17
Erin O'Donell <erinodonnell4@hotmail.com>
2020-04-16 16:26:46 +0100

    .

--- src/main/java/uk/ac/ljmu/fet/cs/cloud/examples/autoscaler/EODAutoScaler.java
+++ src/main/java/uk/ac/ljmu/fet/cs/cloud/examples/autoscaler/EODAutoScaler.java
@@ -1,6 +1,7 @@
 package uk.ac.ljmu.fet.cs.cloud.examples.autoscaler;
 
 import java.util.ArrayList;
+
 import java.util.Iterator;
 
 import hu.mta.sztaki.lpds.cloud.simulator.iaas.IaaSService;
@@ -12,18 +13,18 @@ public class EODAutoScaler extends VirtualInfrastructure {
 	public EODAutoScaler(IaaSService cloud) {
 		super(cloud);
 	}
-
-		//look at again!
 	
 	
 	@Override
 	public void tick(long fires) {
-		// Applications which all need a virtual infrastructure 
+		// Applications which require virtual infrastructure 
 		
 		final Iterator<String> kinds = vmSetPerKind.keySet().iterator();
-		while (kinds.hasNext()) {
+		
+		 while (kinds.hasNext()) {
 			final String kind = kinds.next();
 			final ArrayList<VirtualMachine> vmset = vmSetPerKind.get(kind);
+			
 			if (vmset.size() > 0) {
 				if (vmset.size() < 3) {
 					requestVM(kind);
@@ -37,7 +38,8 @@ public class EODAutoScaler extends VirtualInfrastructure {
 			else for (VirtualMachine vm : vmset) {	
 				if (vm.getState().equals(State.RUNNING)) {
 					
-					// Will destroy the VM if it is empty and there are no tasks.
+					// Will destroy the VM if empty and there are no tasks.
+					
 					if (vm.underProcessing.isEmpty() && vm.toBeAdded.isEmpty()) {
 						destroyVM(vm);
 					}
